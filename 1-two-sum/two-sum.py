7ae061153cb520d1648b28a7495bca96be59e34b
Mittalmerani <138237096+Mittalmerani@users.noreply.github.com>
2026-07-26 18:08:35 +0530

    Time: 1021 ms (40.90%) | Memory: 13.2 MB (35.98%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,6 +1,9 @@
 class Solution:
     def twoSum(self, nums , target):
-     for i in range(len(nums)):
-         need = target - nums[i]
-         if (need in nums and nums.index(need)!=i):
-            return(i , nums.index(need))
+        d=dict()
+        for i in range(len(nums)):
+            d[nums[i]]=i
+        for i in range(len(nums)):
+            need = target - nums[i]
+            if(need in d.keys() and d[need]!= i):
+                return(i,d[need])
